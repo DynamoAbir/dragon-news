71ef1a56fafdce82606bfe94a6f1faf028a1a0ac
DynamoAbir <mhabir10@gmail.com>
2022-11-28 18:24:00 +0600

    added some incones and a carusal

--- src/Pages/Shared/Header/RightSideNav/RightSideNav.js
+++ src/Pages/Shared/Header/RightSideNav/RightSideNav.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaWhatsapp, FaTwitch } from "react-icons/fa";
+import ListGroup from 'react-bootstrap/ListGroup';
+
+import BrandCarusel from '../../BrandCarousel/BrandCarusel';
 
 const RightSideNav = () => {
     return (
         <div>
-            <h2>Right side nav</h2>
+            <ButtonGroup vertical>
+                <Button variant="outline-primary"><FaGoogle></FaGoogle> Login with Google</Button>
+                <Button className='mt-2' variant="outline-dark"> <FaGithub></FaGithub> Login with Github</Button>
+            </ButtonGroup>
+            <div className='mt-5'>
+                <h5>Find Us On</h5>
+                <ListGroup>
+                    <ListGroup.Item className='mb-2'> <FaFacebook /> Facebook</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaWhatsapp /> Whatsapp</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaTwitter /> Twitter</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaTwitch /> Twitch</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'>Vestibulum at eros</ListGroup.Item>
+                </ListGroup>
+            </div>
+            <div>
+                <BrandCarusel></BrandCarusel>
+            </div>
         </div>
+
     );
 };
 
